84c938756c6ac09bb200d35616c146d94980a865
BigZapper <dokodemodoa2112@gmail.com>
2021-09-26 10:10:04 +0700

    Clean React Architecture

--- src/App.js
+++ src/App.js
@@ -1,132 +1,14 @@
 import './App.scss';
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className='trello-ndha'>
-      <nav className='navbar app'>App Bar</nav>
-      <nav className='navbar board'>Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://img-9gag-fun.9cache.com/photo/aXo5Od6_460swp.webp" alt="pic" />
-              Title: NGUYENDONGHA
-            </li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-            <li>Add what you'd like to work to bellow</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
