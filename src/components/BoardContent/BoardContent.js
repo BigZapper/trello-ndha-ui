10eae5b63189f8615fae79d1d38b534b9c6c852a
BigZapper <dokodemodoa2112@gmail.com>
2021-09-26 15:18:57 +0700

    initial sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,17 +1,32 @@
 import Column from 'components/Column/Column';
-import React from 'react';
+import { isEmpty } from 'lodash';
+import React, { useEffect, useState } from 'react';
 import './BoardContent.scss';
+import { initialData } from 'actions/initialData';
+import { mapOrder } from 'ultilities/sorts';
 
 export default function BoardContent() {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+
+    useEffect(() => {
+        const boardFromDB = initialData.boards.find(board => board.id === 'board-1');
+        if (boardFromDB) {
+            setBoard(boardFromDB);
+
+            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
+        }
+    }, [])
+
+    if (isEmpty(board)) {
+        return <div className='not-found'>
+            Board not found!
+        </div>
+    }
+
     return (
         <div className="board-content">
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
+            {columns.map((column, index) => <Column key={index} column={column} />)}
         </div>
     )
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.scss';
+
+export default function Card(props) {
+    const { card } = props;
+
+    return (
+        <li className='card-item'>
+            {card.cover && <img className='card-cover' src={card.cover} alt="pic" />}
+
+            {card.title}
+        </li>
+    )
+}
