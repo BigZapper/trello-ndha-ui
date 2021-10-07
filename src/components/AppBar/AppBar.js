4a294b2c4cd6c286f23456dc643069ed4d29a8f7
BigZapper <dokodemodoa2112@gmail.com>
2021-10-07 09:54:03 +0700

    Design Appbar and Boardbar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap'
+import logo from 'assets/logo.png'
 import './AppBar.scss'
 
 export default function AppBar() {
   return (
-    <nav className='navbar-app'>App Bar</nav>
+    <nav className="navbar-app">
+      <Container className='trello-container'>
+        <Row>
+          <Col sm={5} xs={12} className='col-no-padding'>
+            <div className='app-actions'>
+              <div className='item all'><i className="fa fa-th" /></div>
+              <div className="item home"><i className="fa fa-home"></i></div>
+              <div className="item boards"><i className="fa fa-columns"></i>&nbsp;&nbsp;<strong>Boards</strong></div>
+              <div className="item search">
+                <InputGroup className='group-search'>
+                  <FormControl
+                    className='input-search'
+                    placeholder='Jump to...'
+                  />
+                  <InputGroup.Text className='input-icon-search'><i className="fa fa-search"></i></InputGroup.Text>
+                </InputGroup>
+              </div>
+            </div>
+          </Col>
+          <Col className="col-no-padding" sm={2} xs={12}>
+            <div className="app-branding text-center">
+              <a href="# ">
+                <img src={logo} alt="top-logo" className="top-logo" />
+                <span className="slogan">Trello Clone App</span>
+              </a>
+            </div>
+          </Col>
+          <Col className="col-no-padding" sm={5} xs={12}>
+            <div className="user-actions">
+              <div className="item quick"><i className="fa fa-plus-square-o"></i></div>
+              <div className="item news"><i className="fa fa-info-circle"></i></div>
+              <div className="item notification"><i className="fa fa-bell-o"></i></div>
+              <div className="item user-avatar">
+                <img src="https://lh3.googleusercontent.com/ogw/ADea4I5iWJ0RmxsFyvU_BsOSb9xTvIIF8dZxHFmih2NmFQ=s32-c-mo" alt="avatar" />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </nav>
   )
 }
--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import './BoardBar.scss'
 
 export default function BoardBar() {
   return (
-    <nav className='navbar-board'>Board Bar</nav>
+    <nav className='navbar-board'>
+      <Container className='trello-container'>
+        <Row>
+          <Col className='col-no-padding' sm={10} xs={12}>
+            <div className="board-info">
+              <div className="item board-logo-icon">
+                <i className="fa fa-coffee"></i>&nbsp;&nbsp;<strong>Trello Clone App</strong>
+              </div>
+              <div className="divider"></div>
+
+              <div className="item board-type">Private Workspace</div>
+              <div className="divider"></div>
+
+              <div className="item member-avatar">
+                <img src="https://lh3.googleusercontent.com/a-/AOh14Ghtl7KjFCM0oSII6h9MMvEDY-h59jpMraNjoQIRvQ=s64-c" alt="avatar" />
+                <img src="https://lh3.googleusercontent.com/a-/AOh14Gj50bGQXSnShEklQbHovgBOFvhZ6DbXXY8RRN0deA=s64-c" alt="avatar" />
+                <img src="https://lh3.googleusercontent.com/a-/AOh14GiUIOsgrxmt8jKk9Z_UWGEuOzewY_h0GLd3ZgIYhg=s64-c" alt="avatar" />
+                <img src="https://lh3.googleusercontent.com/a-/AOh14GgrpN5kLpuvVynQg235dPffhzEiJdxi0UK26FkW=s64-c" alt="avatar" />
+                <img src="https://lh3.googleusercontent.com/a-/AOh14GjMMZCLTFHxncGHNONyU-zpS5Y2PM9nMq0Ctxhu=s64-c" alt="avatar" />
+                <span className="more-members">+7</span>
+                <span className='invite'>Invite</span>
+              </div>
+            </div>
+          </Col>
+          <Col className="col-no-paddn" sm={2} xs={12}>
+            <div className="board-actions">
+              <div className="item menu"><i className="fa fa-ellipsis-h mr-2 mt-1"></i> Show menu</div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </nav>
   )
 }
